68fbeb5d1dd5f4eaff74afb644701413bcd9bbe0
kellyleung1 <kelly.leung1@nhsbsa.nhs.uk>
2025-01-28 16:55:34 +0000

    mvp

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -4,4 +4,14 @@ accessibleAutocomplete.enhanceSelectElement({
     selectElement: document.querySelector('#select-1')
   })
 
-  
+  import accessibleAutocomplete from 'accessible-autocomplete';
+
+document.addEventListener('DOMContentLoaded', function () {
+  accessibleAutocomplete.enhanceSelectElement({
+    selectElement: document.querySelector('#council'),
+    defaultValue: '',
+    showAllValues: true, // Allows dropdown to show all values on focus
+    autoselect: false, // Prevents automatic selection
+    dropdownArrow: () => '', // Removes default dropdown arrow
+  });
+});
